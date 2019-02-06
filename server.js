c6bf661e7507571d8005868f82fefa2d274d3eeb
Your Name <you@example.com>
2019-02-06 07:25:09 -0500

    updated recipes get request

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+8
 const express = require('express');
 // we'll use morgan to log the HTTP layer
 const morgan = require('morgan');
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -25,11 +25,36 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// adding 'recipes'
+
+Recipes.create(
+  'Guacamole', ['3 avocados (ripe)',
+    '1 small onion',
+    '2 Roma tomatoes', 
+    '3 tbsp, fresh cilantro',
+    '1 jalapeno pepper',
+    '2 garlic cloves', 
+    '1 lime', 
+    '1/2 tsp sea salt']);
+
+    Recipes.create(
+      'Mole', [ '2 cups broth', '2 dried guajillo chiles', '2 dried ancho chiles', 
+        '3 dried chipotle chiles', '2 corn tortillas', '2 tomatoes', 
+       '5 tomatillos']
+    );
+
+
 // when the root of this route is called with GET, return
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+});
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
